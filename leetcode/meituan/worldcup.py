9c3966430347575f1e85b5a71dde5659346b9f95
xiaoke <xiaoke3154@foxmail.com>
2018-05-28 20:57:25 +0800

    BST

--- leetcode/meituan/worldcup.py
+++ leetcode/meituan/worldcup.py
@@ -4,3 +4,14 @@ if __name__ == "__main__":
     rate = []
     for i in range(16):
         nums = map(float, sys.stdin.readline().strip().split())
+        rate.append(nums)
+    finaleight = []
+    finalfour = []
+    finaltwo = []
+    # 先计算八分之一决赛
+    for i in range(8):
+        finaleight[2 * i] = rate[2 * i][2 * i + 1]
+        finaleight[2 * i + 1] = rate[2 * i + 1][2 * i]
+
+    # 再计算四强：
+    # for j in range(4):
